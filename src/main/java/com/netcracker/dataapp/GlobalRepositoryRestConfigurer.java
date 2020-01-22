c712cff5f34529cef5337054e150440a297ccb9b
Evgenii Komrakov <evgenii.komrakov@netcracker.com>
2020-01-22 09:34:39 +0300

    Allow CORS

---
+++ src/main/java/com/netcracker/dataapp/GlobalRepositoryRestConfigurer.java
@@ -0,0 +1,18 @@
+package com.netcracker.dataapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class GlobalRepositoryRestConfigurer implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getCorsRegistry()
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
