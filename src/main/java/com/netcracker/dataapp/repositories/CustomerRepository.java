03326bb5f9e594264ac1df3c771ab6b3d5afa870
Evgenii Komrakov <evgenii.komrakov@netcracker.com>
2020-01-22 16:27:21 +0300

    Api to get all Customers by serviceStatusId

--- src/main/java/com/netcracker/dataapp/repositories/CustomerRepository.java
+++ src/main/java/com/netcracker/dataapp/repositories/CustomerRepository.java
@@ -1,10 +1,14 @@
 package com.netcracker.dataapp.repositories;
 
 import com.netcracker.dataapp.entities.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     Optional<Customer> getCustomerByPhoneNumber(String phoneNumber);
+
+    @Query("select c from Customer c, Service s where s.customer.id = c.id and s.serviceStatus.id = ?1")
+    Iterable<Customer> findAllByServiceStatus_Id(Long serviceStatusId);
 }
