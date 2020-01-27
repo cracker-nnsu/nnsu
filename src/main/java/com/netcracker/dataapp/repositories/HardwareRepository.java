e5ac2ded9c0579664ca2f7a6a11085cdddc58983
Evgenii Komrakov <evgenii.komrakov@netcracker.com>
2020-01-27 15:36:55 +0300

    Fix bugs

--- src/main/java/com/netcracker/dataapp/repositories/HardwareRepository.java
+++ src/main/java/com/netcracker/dataapp/repositories/HardwareRepository.java
@@ -4,57 +4,48 @@ import com.netcracker.dataapp.entities.*;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+
 public interface HardwareRepository extends CrudRepository<Hardware, Long> {
-    Iterable<Hardware> findAllByAddress(Address address);
     Iterable<Hardware> findAllByAddress_Id(Long addressId);
-    Iterable<Hardware> findAllByService(Service service);
     Iterable<Hardware> findAllByService_Id(Long serviceId);
     Iterable<Hardware> findAllByServiceIsNull();
-    Iterable<Hardware> findAllByHardwareStatus(HardwareStatus hardwareStatus);
     Iterable<Hardware> findAllByHardwareStatus_Id(Long hardwareStatusId);
 
-    @Query("select h from Hardware h where h.hardwareStatus.id = 2 and h.service.customer = ?1")
-    Iterable<Hardware> findAllFaultyByCustomer(Customer customer);
     @Query("select h from Hardware h where h.hardwareStatus.id = 2 and h.service.customer.id = ?1")
     Iterable<Hardware> findAllFaultyByCustomer_Id(Long customerId);
 
-    @Query("select h from Hardware h where h.hardwareStatus.id = 4 and h.service.customer = ?1")
-    Iterable<Hardware> findAllToReplaceByCustomer(Customer customer);
     @Query("select h from Hardware h where h.hardwareStatus.id = 4 and h.service.customer.id = ?1")
     Iterable<Hardware> findAllToReplaceByCustomer_Id(Long customerId);
 
-    @Query("select h from Hardware h where h.service.customer = ?1 and h.hardwareStatus.id = ?2")
-    Iterable<Hardware> findAllByCustomerAndHardwareStatus(Customer customer, HardwareStatus hardwareStatus);
     @Query("select h from Hardware h where h.service.customer.id = ?1 and h.hardwareStatus.id = ?2")
     Iterable<Hardware> findAllByCustomer_IdAndHardwareStatus_Id(Long customerId, Long hardwareStatusId);
 
-    @Query("select h from Hardware h where h.hardwareStatus.id = 2 and h.service = ?1")
-    Iterable<Hardware> findAllFaultyByService(Service service);
     @Query("select h from Hardware h where h.hardwareStatus.id = 2 and h.service.id = ?1")
     Iterable<Hardware> findAllFaultyByService_Id(Long serviceId);
 
     @Modifying
     @Transactional
-    @Query("update Hardware h set h.hardwareStatus = ?1 where h = ?2")
-    void setHardwareStatusByHardware(HardwareStatus hardwareStatus, Hardware hardware);
-    @Modifying
-    @Transactional
     @Query("update Hardware h set h.hardwareStatus.id = ?1 where h.id = ?2")
-    void setHardwareStatusByHardware_Id(Long hardwareStatusId, Long hardwareId);
+    int setHardwareStatusByHardware_Id(Long hardwareStatusId, Long hardwareId);
 
     @Modifying
     @Transactional
     @Query("update Hardware h set h.hardwareStatus.id = 1 where h.id = ?1")
-    void setHardwareStatusNormalByHardware_Id(Long hardwareId);
+    int setHardwareStatusNormalByHardware_Id(Long hardwareId);
+    @Modifying
+    @Transactional
+    @Query(value = "update hardware set hardware_status_id = 2 where id = :hardwareId ; update service set service_status_id = 2 where id = (select h.service_id from hardware h where h.id = :hardwareId )", nativeQuery = true)
+    int setHardwareStatusFailByHardware_Id(@Param("hardwareId") Long hardwareId);
     @Modifying
     @Transactional
     @Query("update Hardware h set h.hardwareStatus.id = 3 where h.id = ?1")
-    void setHardwareStatusMaintenanceByHardware_Id(Long hardwareId);
+    int setHardwareStatusUnderMaintByHardware_Id(Long hardwareId);
 
     @Modifying
     @Transactional
-    @Query(value = "update hardware set service_id = (select old_hw.service_id from hardware old_hw where old_hw.id = ?1) where id = ?2; update hardware set service_id = null where id = ?1", nativeQuery = true)
-    void replaceHardware(Long oldHardwareId, Long newHardwareId);
+    @Query(value = "update hardware set service_id = (select old_hw.service_id from hardware old_hw where old_hw.id = :oldHardwareId ) where id = :newHardwareId ; update hardware set service_id = null where id = :oldHardwareId ", nativeQuery = true)
+    int replaceHardware(@Param("oldHardwareId") Long oldHardwareId, @Param("newHardwareId") Long newHardwareId);
 }
--- src/main/java/com/netcracker/dataapp/repositories/ServiceRepository.java
+++ src/main/java/com/netcracker/dataapp/repositories/ServiceRepository.java
@@ -9,22 +9,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface ServiceRepository extends CrudRepository<Service, Long> {
-    Iterable<Service> findAllByCustomer(Customer customer);
     Iterable<Service> findAllByCustomer_Id(Long customerId);
-    Iterable<Service> findAllByServiceStatus(ServiceStatus serviceStatus);
     Iterable<Service> findAllByServiceStatus_Id(Long serviceStatusId);
 
     @Modifying
     @Transactional
-    @Query("update Service s set s.serviceStatus = ?1 where s.customer = ?2 and s = ?3")
-    void setServiceStatusByCustomerAndService(ServiceStatus serviceStatus, Customer customer, Service service);
-    @Modifying
-    @Transactional
     @Query("update Service s set s.serviceStatus.id = ?1 where s.customer.id = ?2 and s.id = ?3")
-    void setServiceStatusByCustomer_IdAndService_Id(Long serviceStatusId, Long customerId, Long serviceId);
+    int setServiceStatusByCustomer_IdAndService_Id(Long serviceStatusId, Long customerId, Long serviceId);
 
     @Modifying
     @Transactional
     @Query(value = "update service set service_status_id = 2 where customer_id = ?1 and id = ?2; update hardware set hardware_status_id = 2 where service_id = ?2 order by RAND() limit 1", nativeQuery = true)
-    void setServiceStatusFailByCustomer_IdAndService_Id(Long customerId, Long serviceId);
+    int setServiceStatusFailByCustomer_IdAndService_Id(Long customerId, Long serviceId);
 }
